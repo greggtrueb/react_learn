c6a5bfc2cd2204c90461d4f45b16591eae50e095
Gregg Trueb <GreggTrueb@gmail.com>
2020-06-16 00:23:17 -0400

    adding some stuff

---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+    return {
+        Categories: ["Bombs", "Missiles", "Sex Weapons"],
+        Products: [
+            {id: 1, name: "Buster", category: "Bombs", description: "Big boom", price: 700000},
+            {id: 2, name: "Breaker", category: "Bombs", description: "Medium boom", price: 500000},
+            {id: 3, name: "Cracker", category: "Bombs", description: "Small boom", price: 300000},
+            {id: 4, name: "Cracker", category: "Bombs", description: "Small boom", price: 300000},
+            {id: 5, name: "Lance", category: "Missiles", description: "Far away boom", price: 7000000},
+            {id: 6, name: "Arrow", category: "Missiles", description: "Sorta away boom", price: 5000000},
+            {id: 7, name: "Stick", category: "Missiles", description: "Close away boom", price: 3000000},
+            {id: 8, name: "Gregg's Thunder", category: "Sex Weapons", description: "You can't handel it", price: 70},
+            {id: 8, name: "The Satisfied", category: "Sex Weapons", description: "It'll get you there", price: 50},
+            {id: 8, name: "The Ehh Whatever", category: "Sex Weapons", description: "Ehh whatever", price: 30}
+        ],
+        Orders: []
+    }
+}
