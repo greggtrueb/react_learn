9e23d8ffd819b60050d6f444c584bec1b44d4899
Gregg Trueb <GreggTrueb@gmail.com>
2020-07-09 23:51:12 -0400

    page 117

--- src/data/placeholderData.js
+++ src/data/placeholderData.js
@@ -7,7 +7,7 @@ export const data = {
         {id: 4, name: "Lance", category: "Missiles", description: "Far away boom", price: 7000000},
         {id: 5, name: "Arrow", category: "Missiles", description: "Sorta away boom", price: 5000000},
         {id: 6, name: "Stick", category: "Missiles", description: "Close away boom", price: 3000000},
-        {id: 7, name: "Gregg's Thunder", category: "Sex Weapons", description: "You can't handel it", price: 70},
+        {id: 7, name: "Gregg's Thunder", category: "Sex Weapons", description: "You can't handel it", price: 7000000000},
         {id: 8, name: "The Satisfied", category: "Sex Weapons", description: "It'll get you there", price: 50},
         {id: 9, name: "The Ehh Whatever", category: "Sex Weapons", description: "Ehh whatever", price: 30}
     ]
